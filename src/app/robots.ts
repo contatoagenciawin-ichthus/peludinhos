5ff8e0397e50ba30338ccd3e144becfdd7fc4bdb
Ichthus Mkt - Agência Win <contatoagenciawin@gmail.com>
2026-08-25 10:11:35 -0300

    Libera Googlebot e publica sitemap no robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,8 +4,13 @@ import { SITE_LIVE, SITE_URL } from "@/lib/site-data";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: SITE_LIVE
-      ? { userAgent: "*", allow: "/" }
+      ? [
+          { userAgent: "*", allow: "/" },
+          { userAgent: "Googlebot", allow: "/" },
+          { userAgent: "Googlebot-Image", allow: "/" },
+        ]
       : { userAgent: "*", disallow: "/" },
     sitemap: SITE_LIVE ? `${SITE_URL}/sitemap.xml` : undefined,
+    host: SITE_LIVE ? SITE_URL : undefined,
   };
 }
